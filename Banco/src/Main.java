520175364f86ab87b4b183282aad1a268afb2d01
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-06-22 15:33:52 -0300

    Implementação do metodo cheque especial.

--- Banco/src/Main.java
+++ Banco/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Banco banco = new Banco("José", 1000.0,500.0);
+        Banco banco = new Banco("José", 1000,600);
         banco.exibirSaldo();
         banco.sacar(500.0);
         banco.exibirSaldo();
